7c00e12e29235e24f6995e6b9bc27b4688929583
Vlad Okhotnikov <rettakecsgo@gmail.com>
2021-12-28 18:49:52 +0300

    Вторая итерация

--- script/script.js
+++ script/script.js
@@ -1,36 +1,33 @@
 let popup = document.querySelector('.popup');
 let formElement = popup.querySelector('.popup__input');
-let nameInput = popup.querySelector('.popup__name');
-let jobInput = popup.querySelector('.popup__job');
+let nameInput = popup.querySelector('input[name="userName"]');
+let jobInput = popup.querySelector('input[name="userJob"]');
 let editButton = document.querySelector('.profile__edit');
 let closeButton = popup.querySelector('.popup__close');
-let inputs = popup.querySelectorAll('input');
-let profileName = document.querySelector('.popup__text-area_value-name');
-let profileJob = document.querySelector('.popup__text-area_value-job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
-function formOpen() {
+console.log(nameInput);
+
+function popupOpen() {
    popup.classList.add('popup_opened');
 }
 
-function formClose() {
+function popupClose() {
    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
    evt.preventDefault();
-
-   let name = inputs[0].value;
-   let job = inputs[1].value;
    
+   profileName.textContent = nameInput.value; 
+   profileJob.textContent = jobInput.value; 
 
-   profileName.textContent = name;
-   profileJob.textContent = job;
-   
-   formClose();
+   popupClose();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-closeButton.addEventListener('click', formClose);
-editButton.addEventListener('click', formOpen);
+closeButton.addEventListener('click', popupClose);
+editButton.addEventListener('click', popupOpen);
 formElement.addEventListener('submit', formSubmitHandler);
