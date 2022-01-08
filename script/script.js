3084c715854ae3690ae05d0730ff40c99202b345
Vlad Okhotnikov <rettakecsgo@gmail.com>
2022-01-08 17:55:40 +0300

    Image delete add

--- script/script.js
+++ script/script.js
@@ -37,6 +37,33 @@ function formSubmitHandler(evt) {
    popupClose();
 }
 
+const initialCards = [
+   {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+   },
+   {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+   },
+   {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+   },
+   {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+   },
+   {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+   },
+   {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+   }
+ ]; 
+
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 closeButton.addEventListener('click', popupClose);
